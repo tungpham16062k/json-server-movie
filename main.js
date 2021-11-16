10b0020f7672ff4aaa7de4d365eb1fff4cc56032
unknown <tungpham16062k@gmail.com>
2021-11-16 21:39:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -24,8 +24,7 @@ server.use((req, res, next) => {
 
 // Use default router
 server.use(router)
-
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
+console.log('Ready to start server');
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
